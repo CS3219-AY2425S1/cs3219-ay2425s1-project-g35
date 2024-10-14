0cb409acb00410eae1998eb25d59a8127f3aedb4
Alexander Setyawan <alexgenrong@gmail.com>
2024-10-14 19:33:53 +0800

    Add queue

--- MatchingService/routes/matcher.js
+++ MatchingService/routes/matcher.js
@@ -6,70 +6,87 @@ const waitingTime = 60000;
 
 // hash table of waiting users
 const criteriaToUsers = new Map();
+const queue = [];
+
+function updateQueue() {
+    while (queue.length > 0) {
+        queueElement = queue.shift();
+        criteria = queueElement['criteria']
+        if (criteriaToUsers.contains(criteria)) {
+            userId1 = queueElement['userId']
+            userId2 = criteriaToUsers.get(criteria);
+
+            // delete other user from hash table
+            criteriaToUsers.delete(criteria);
+
+            // send event signifying match found
+            eventEmitter.emit('matchFound', { user1: userId1, user2: userId2 });
+
+            // FOR FUTURE CONSIDERATION (collab service logic)
+            // if question id not null, go to waiting room
+            // if null, pick random question based on diffLevel and topic
+        } else {
+            criteriaToUsers.set(criteria, queueElement['userId']);
+        }
+    }
+}
 
 router.get('/', (req, res) => {
     // Returns queue view with the current queue data
+    updateQueue();
     res.json(criteriaToUsers);
 });
 
 router.post('/', (req, res) => {
-    mainUserId = req.query.userId
-    key = {
+    userIdVar = req.query.userId
+    if (isNaN(userIdVar)) {
+        res.status(400).json({'error': 'Invalid User ID'});
+        return;
+    }
+
+    criteriaVar = {
         questionId: req.query.questionId,
         topic: req.query.topic,
         diffLevel: req.query.diffLevel
-    };
-
-    if (criteriaToUsers.has(key)) {
-        // get details of other user
-        otherUserId = criteriaToUsers.get(key);
-        
-        // delete other user from hash table
-        criteriaToUsers.delete(key);
-
-        // send event signifying match found
-        eventEmitter.emit('matchFound', { userId1: mainUserId, userId2: otherUserId });
-        
-        res.json({ message: "Match found!", otherUserId });
-        
-        // FOR FUTURE CONSIDERATION (collab service logic)
-        // if question id not null, go to waiting room
-        // if null, pick random question based on diffLevel and topic
-    } else {
-        criteriaToUsers.set(key, mainUserId);
-
-        const timeoutId = setTimeout(() => {
-            // Check if user is still in the map
-            if (criteriaToUsers.has(key) && criteriaToUsers.get(key) == mainUserId) {
-                // User did not find match
-                criteriaToUsers.delete(key);
-                
-                // Send error message indicating no match found
-                res.status(404).json({ message: "No match found within time limit.", key });
-            }
-        }, waitingTime);
-        
-        // Listen for the 'matchFound' event and clear the timeout if it occurs
-        eventEmitter.on('matchFound', (data) => {
-            // Check if the event relates to the current user
-            userId1 = data.userId1;
-            userId2 = data.userId2;
-            if (userId1 == mainUserId) {
-                clearTimeout(timeoutId);
-                res.json({ message: "Match found!", userId2 });
-            } else if (userId2 == mainUserId) {
-                clearTimeout(timeoutId);
-                res.json({ message: "Match found!", userId1 });
-            }
-        });
-
-        // FOR FUTURE CONSIDERATION (collab service logic)
-        // if question id not null, go to waiting room
-        // if null, pick random question based on diffLevel and topic
     }
+    queueElement = {
+        userId: userIdVar,
+        criteria: criteriaVar
+    }
+
+    queue.push(queueElement);
+    console.log(`User ${userIdVar} enqueued`);
+
+    updateQueue();
+
+    const timeoutId = setTimeout(() => {
+        // Check if user is still in the map
+        if (criteriaToUsers.has(criteriaVar) && criteriaToUsers.get(criteriaVar) == userIdVar) {
+            // User did not find match
+            console.log(`Match not found for ${userIdVar}!`);
+            res.json({ message: "Match not found!", userIdVar });
+        }
+    }, waitingTime);
+    
+    // Listen for the 'matchFound' event and clear the timeout if it occurs
+    eventEmitter.on('matchFound', (data) => {
+        // Check if the event relates to the current user
+        userId1 = data.userId1;
+        userId2 = data.userId2;
+        if (userId1 == userIdVar) {
+            clearTimeout(timeoutId);
+            console.log(`Match found with ${userId2}!`);
+            res.json({ message: `Match found with ${userId2}!`});
+        } else if (userId2 == userIdVar) {
+            clearTimeout(timeoutId);
+            console.log(`Match found with ${userId1}!`);
+            res.json({ message: `Match found with ${userId1}!`});
+        }
+    });
 });
 
 router.delete('/', (req, res) => {
+    userId = req.query.userId;
     key = {
         questionId: req.query.questionId,
         topic: req.query.topic,
@@ -80,6 +97,7 @@ router.delete('/', (req, res) => {
         criteriaToUsers.delete(key);
         res.json({ message: 'Key deleted successfully', key });
     } else {
+        queue.filter(user => user['userId'] != userId);
         res.json({ message: 'Key not found', key });
     }
 });
