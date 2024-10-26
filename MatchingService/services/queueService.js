a8c7a37e3d6a3412c230934d50c18c1c57d184ef
lambraydon <braydonlam2001@gmail.com>
2024-10-26 13:46:46 +0800

    Add publish and consume methods for match found channel

--- MatchingService/services/queueService.js
+++ MatchingService/services/queueService.js
@@ -1,25 +1,30 @@
 import connectRabbitMQ from '../config/rabbitmq.js';
 import QueueModel from '../models/queue-model.js';
 
-const QUEUE_NAME = 'match_requests';
+const MATCH_REQUEST_CHANNEL = 'match_requests';
+const MATCH_FOUND_CHANNEL = 'match_found';
+
+async function getChannel() {
+    const { channel } = await connectRabbitMQ();
+    return channel;
+}
 
 async function publishMatchRequest(message) {
-    const { channel } = await connectRabbitMQ();
-    await channel.assertQueue(QUEUE_NAME, { durable: false });
-    channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)));
+    const channel = await getChannel();
+    await channel.assertQueue(MATCH_REQUEST_CHANNEL, { durable: false });
+    channel.sendToQueue(MATCH_REQUEST_CHANNEL, Buffer.from(JSON.stringify(message)));
 
-    console.log('Published message to RabbitMQ:', message);
+    console.log(`Published message to RabbitMQ ${MATCH_REQUEST_CHANNEL} channel:`, message);
 }
 
 async function consumeMatchRequests(processMessage) {
     try {
-        const { channel } = await connectRabbitMQ();
-        await channel.assertQueue(QUEUE_NAME, { durable: false });
+        const channel = await getChannel();
+        await channel.assertQueue(MATCH_REQUEST_CHANNEL, { durable: false });
 
-        channel.consume(QUEUE_NAME, async (msg) => {
+        channel.consume(MATCH_REQUEST_CHANNEL, async (msg) => {
             if (msg !== null) {
                 const message = JSON.parse(msg.content.toString());
-
                 const { userId } = message;
 
                 // Check if the user is still in the Redis queue
@@ -28,21 +33,59 @@ async function consumeMatchRequests(processMessage) {
                 console.log('Consumed message from RabbitMQ:', message);
 
                 try {
-                    // If not in Redis queue means that the user either cancelled or timeout, skip processing
                     if (isUserInQueue) {
                         await processMessage(message);
                     }
                     channel.ack(msg);
                 } catch (error) {
                     console.log(error);
-                    throw new Error('Error processing message:');
+                    throw new Error(`Error processing message from ${MATCH_REQUEST_CHANNEL}`);
                 }
             }
         });
     } catch (error) {
         console.log(error);
-        throw new Error('Error consuming message');
+        throw new Error(`Error consuming message from ${MATCH_REQUEST_CHANNEL}`);
     }
 }
 
-export default { publishMatchRequest, consumeMatchRequests };
+async function publishMatchFound(message) {
+    const channel = await getChannel();
+    await channel.assertQueue(MATCH_FOUND_CHANNEL, { durable: false });
+    channel.sendToQueue(MATCH_FOUND_CHANNEL, Buffer.from(JSON.stringify(message)));
+
+    console.log(`Published message to RabbitMQ ${MATCH_FOUND_CHANNEL} channel:`, message);
+}
+
+async function consumeMatchFound(processMessage) {
+    try {
+        const channel = await getChannel();
+        await channel.assertQueue(MATCH_FOUND_CHANNEL, { durable: false });
+
+        channel.consume(MATCH_FOUND_CHANNEL, async (msg) => {
+            if (msg !== null) {
+                const message = JSON.parse(msg.content.toString());
+
+                console.log('Consumed message from RabbitMQ:', message);
+
+                try {
+                    await processMessage(message);
+                    channel.ack(msg);
+                } catch (error) {
+                    console.log(error);
+                    throw new Error(`Error processing message from ${MATCH_FOUND_CHANNEL}`);
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error(`Error consuming message from ${MATCH_FOUND_CHANNEL}`);
+    }
+}
+
+export default {
+    publishMatchRequest,
+    consumeMatchRequests,
+    publishMatchFound,
+    consumeMatchFound,
+};
