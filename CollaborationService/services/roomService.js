9162db3edade4ec6f4afcc2509ca81c279f70e4a
lambraydon <braydonlam2001@gmail.com>
2024-10-26 18:45:15 +0800

    Add room model and service

---
+++ CollaborationService/services/roomService.js
@@ -0,0 +1,27 @@
+import Room from '../models/room-model.js';
+
+const rooms = {}; 
+
+function createRoom(roomId, user1, user2, topic, difficulty) {
+    // Sample hardcoded question
+    const question = {
+        "Question ID": 101,
+        "Question Title": "Two Sum",
+        "Question Description": "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
+        "Question Categories": ["Array", "Hash Table"],
+        "Link": "https://leetcode.com/problems/two-sum/",
+        "Question Complexity": "Easy"
+    }
+    
+    rooms[roomId] = new Room(roomId, user1, user2, question);
+    return rooms[roomId];
+}
+
+function getRoom(roomId) {
+    return rooms[roomId];
+}
+
+export default {
+    createRoom,
+    getRoom
+}
