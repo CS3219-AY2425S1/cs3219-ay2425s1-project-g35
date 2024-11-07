8fee063ce7484ff27f6b18af25299fe4ed285489
Chow Yuan Jing <zcyuanjing@gmail.com>
2024-11-08 00:30:37 +0800

    Try hacky fix for Github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
   base: '/cs3219-ay2425s1-project-g35/'
 })
