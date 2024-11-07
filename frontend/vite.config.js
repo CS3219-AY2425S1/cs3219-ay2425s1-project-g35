53cbada2924ca62e4389de6603ea9aea16e4a918
Chow Yuan Jing <zcyuanjing@gmail.com>
2024-11-08 00:09:20 +0800

    Fix: Try ./ as base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cs3219-ay2425s1-project-g35/'
+  // base: '/cs3219-ay2425s1-project-g35/'
+  base: './'
 })
